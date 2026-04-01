57c94b87aa746316f22025082fa5871d59446ff0
Aditya Vandan Sharma <van13heisenberg@gmail.com>
2026-04-02 00:59:36 +0530

    feat: leetcode-session 2 (#5)

---
+++ ds-algo/leetcode/17_letter_combinations_of_a_phone_number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def __init__(self):
+        self.cache = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+    def letterCombinations(self, digits: str) -> List[str]:
+        result = []
+        for digit in digits:
+            characters = self.cache[digit]
+            current=[val for val in result]
+            if len(current) == 0: result = [character for character in characters]
+            else:
+                newResult = []
+                for character in characters:
+                    for val in current:
+                        newResult.append(val+character)
+                result = newResult
+        
+        return result
---
+++ ds-algo/leetcode/40_combination_sum_II.py
@@ -0,0 +1,21 @@
+class Solution:
+    def recursion(self, candidates: List[int], target: int, start: int, current_result: List[int], result: List[List[int]]):
+        if target < 0: return
+        if target == 0:
+            print(current_result, start)
+            result.append(current_result[:])
+            return
+        for i in range(start, len(candidates)):
+            if i > start and candidates[i] == candidates[i - 1]:
+                continue
+            if candidates[i] > target:
+                break
+            current_result.append(candidates[i])
+            self.recursion(candidates, target - candidates[i], i + 1, current_result, result)
+            current_result.pop()
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        input_candidates = candidates[:]
+        input_candidates.sort()
+        self.recursion(input_candidates, target, 0, [],result)
+        return result
